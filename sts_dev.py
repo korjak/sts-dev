9657b56b15bc29720e687b9ea73f03d6a652ab1b
korjak <korthalsjakub@gmail.com>
2020-11-19 22:02:14 +0000

    Changes is sts_dev

--- sts_dev.py
+++ sts_dev.py
@@ -5,7 +5,7 @@ import csv
 import torch
 import os
 import re
-
+import time
 
 from transformers import (
     AutoTokenizer,
@@ -25,8 +25,16 @@ def compute_pearson(x, y):
     return pearsonr(x, y)[0]
 
 
+def extract_number(f):
+    s = re.findall("\d+$", f)
+    return (int(s[0]) if s else -1, f)
+
+
 def eval_hf(
-    model_name: str, set_types: str = "all", model_dictionary: str = "./hf_models"
+    model_name: str,
+    set_types: str = "all",
+    model_dictionary: str = "./hf_models",
+    fixed_checkpoint_no: int = None,
 ):
 
     data_args = GlueDataTrainingArguments(task_name="sts-b", data_dir="./data/STS-B")
@@ -38,7 +46,6 @@ def eval_hf(
 
     train_args = TrainingArguments(
         do_eval=True,
-        output_dir=f"./hf_eval/{model_name}",
     )
 
     def build_compute_metrics_fn(task_name: str) -> Callable[[EvalPrediction], Dict]:
@@ -49,14 +56,21 @@ def eval_hf(
 
         return compute_metrics_fn
 
+    checkpoints = next(os.walk(checkpoint_path + model_name))[1]
+    if fixed_checkpoint_no is not None:
+        checkpoint_no = fixed_checkpoint_no
+    else:
+        checkpoint_no = max(checkpoints, key=extract_number)
+    model_path = (
+        checkpoint_path + model_name + "/" + str(checkpoint_no)
+    )
+
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     train_dataset = GlueDataset(data_args, tokenizer=tokenizer)
-    train_test_dataset = GlueDataset(data_args, tokenizer=tokenizer, mode="train")
     eval_dataset = GlueDataset(data_args, tokenizer=tokenizer, mode="dev")
-    test_dataset = GlueDataset(data_args, tokenizer=tokenizer, mode="test")
 
     model = AutoModelForSequenceClassification.from_pretrained(
-        model_name,
+        model_path,
         config=config,
     )
     trainer = Trainer(
@@ -66,14 +80,9 @@ def eval_hf(
     )
 
     return {
-        "train": trainer.evaluate(eval_dataset=train_test_dataset),
+        "train": trainer.evaluate(eval_dataset=train_dataset),
         "dev": trainer.evaluate(eval_dataset=eval_dataset),
     }
-
-
-def extract_number(f):
-    s = re.findall("\d+$", f)
-    return (int(s[0]) if s else -1, f)
 
 
 def train_hf(
@@ -84,61 +93,76 @@ def train_hf(
     from_checkpoint=False,
     checkpoint_path="./hf_models/",
 ) -> Dict[str, float]:
-    if data_args is None:
-        data_args = GlueDataTrainingArguments(
-            task_name="sts-b", data_dir="./data/STS-B"
-        )
-    if config is None:
-        config = AutoConfig.from_pretrained(
-            model_name,
-            num_labels=1,
-            finetuning_task="sts-b",
-        )
-    if train_args is None:
-        train_args = TrainingArguments(
-            do_train=True,
-            do_eval=True,
-            num_train_epochs=5.0,
-            output_dir=f"./hf_models/{model_name}",
-        )
-
-    def build_compute_metrics_fn(task_name: str) -> Callable[[EvalPrediction], Dict]:
-        def compute_metrics_fn(p: EvalPrediction):
-            return glue_compute_metrics(
-                task_name, np.squeeze(p.predictions), p.label_ids
+    name_only = model_name
+    try:
+        tic = time.time()
+        if data_args is None:
+            data_args = GlueDataTrainingArguments(
+                task_name="sts-b", data_dir="./data/STS-B"
+            )
+        if config is None:
+            config = AutoConfig.from_pretrained(
+                model_name,
+                num_labels=1,
+                finetuning_task="sts-b",
+            )
+        if train_args is None:
+            train_args = TrainingArguments(
+                evaluate_during_training=True,
+                do_train=True,
+                do_eval=True,
+                num_train_epochs=5.0,
+                output_dir=f"/data/hf_models/{model_name.replace('/', '_')}",
             )
 
-        return compute_metrics_fn
+        def build_compute_metrics_fn(
+            task_name: str,
+        ) -> Callable[[EvalPrediction], Dict]:
+            def compute_metrics_fn(p: EvalPrediction):
+                return glue_compute_metrics(
+                    task_name, np.squeeze(p.predictions), p.label_ids
+                )
 
-    # here we still need model_name to be solely a name, not a path
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    train_dataset = GlueDataset(data_args, tokenizer=tokenizer)
-    eval_dataset = GlueDataset(data_args, tokenizer=tokenizer, mode="dev")
+            return compute_metrics_fn
 
-    # with this condition we can change model_name to the path to the latest checkpoint
-    if from_checkpoint == True:
-        # we search for every checkpoint created for our model
-        checkpoints = next(os.walk(checkpoint_path + model_name))[1]
-        # and set model_name as the path to the one with highest number
-        model_name = (
-            checkpoint_path
-            + model_name
-            + "/"
-            + str(max(checkpoints, key=extract_number))
+        # here we still need model_name to be solely a name, not a path
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        train_dataset = GlueDataset(data_args, tokenizer=tokenizer)
+        eval_dataset = GlueDataset(data_args, tokenizer=tokenizer, mode="dev")
+
+        # with this condition we can change model_name to the path to the latest checkpoint
+        if from_checkpoint == True:
+            # we search for every checkpoint created for our model
+            checkpoints = next(os.walk(checkpoint_path + model_name))[1]
+            # and set model_name as the path to the one with highest number
+            model_name = (
+                checkpoint_path
+                + model_name
+                + "/"
+                + str(max(checkpoints, key=extract_number))
+            )
+
+        model = AutoModelForSequenceClassification.from_pretrained(
+            model_name,
+            config=config,
+        )
+        trainer = Trainer(
+            model=model,
+            args=train_args,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            compute_metrics=build_compute_metrics_fn("sts-b"),
         )
 
-    model = AutoModelForSequenceClassification.from_pretrained(
-        model_name,
-        config=config,
-    )
-    trainer = Trainer(
-        model=model,
-        args=train_args,
-        train_dataset=train_dataset,
-        compute_metrics=build_compute_metrics_fn("sts-b"),
-    )
+        trainer.train(model_path=model_name)
+        trainer.save_model()
+        t = time.time() - tic
 
-    trainer.train(model_path=model_name)
-    trainer.save_model()
-
-    return trainer.evaluate(eval_dataset=eval_dataset)
+        return {
+            "model_name": name_only,
+            "train": trainer.evaluate(eval_dataset=train_dataset),
+            "dev": trainer.evaluate(eval_dataset=eval_dataset),
+            "time": t,
+        }
+    except Exception as exe:
+        return {"model_name": name_only, "exception": str(exe)}
